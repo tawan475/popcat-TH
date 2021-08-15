20688fdb3f993d7b850155ba2d6d4b860ed4c820
tawan475 <43491902+tawan475@users.noreply.github.com>
2021-08-15 07:29:11 +0700

    1.1.0
    Tested that the limitation are per IP and not instance
    Simplify the program
    Update Installation

---
+++ START.bat
@@ -0,0 +1,3 @@
+@echo off
+title popcat.click bot by tawan475
+node popper.js
--- popper.js
+++ popper.js
@@ -1,52 +1,43 @@
-process.setMaxListeners(0);
 const puppeteer = require("puppeteer");
-const fs = require("fs");
-let limiter = parseInt(process.argv.slice(2)[0]);
+let counter = 0;
 
-let screenshots = "./screenshots/"
+(async () => {
+    console.log("Starting!")
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36")
 
-let counter = 0
-app = () => {
-    (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36")
-        await page.setViewport({ width: 300, height: 250 });
-        await page.goto("https://popcat.click/");
+    page.on('requestfinished', async (request) => {
+        if (!request.url().startsWith("https://stats.popcat.click/pop?pop_count=")) return
+        let rawResponse = await request.response();
+        let response = await rawResponse.buffer();
+        let responseBody = response.toString();
+        
+        if (responseBody.startsWith("429")) return console.log("Too many request! Did you forget to close the browser? retrying 30 seconds.")
+        var json = JSON.parse(responseBody);
+        console.log("Popped for " + json["Location"]["Name"] + "! x" + ++counter)
+    });
 
-        let i = 0;
-        let ID = (counter++).toString()
-        console.log(ID.padStart(2, " ") + ' ready!');
-        setInterval(async () => {
-            if (counter !== limiter) return
-            console.log(ID.padStart(2, " ") + ":clicked");
-            await page.evaluate(() => {
-                for (i = 0; i < 800; i++) {
-                    document.dispatchEvent(new KeyboardEvent('keydown', {
-                        key: 'x',
-                        ctrlKey: true
-                    }));
-                }
-            });
-        }, 1000 * 30);
-        setInterval(async () => {
-            await page.screenshot({ path: `./screenshots/${ID}.png` });
-        }, 1000 * 60);
-    })()
-}
 
-for (let i = 0; i < limiter; i++) {
-    app();
-}
-
-if (!fs.existsSync(screenshots)) {
-    fs.mkdirSync(screenshots);
-} else {
-    let folder = fs.readdirSync(screenshots)
-    for (const file of folder) {
-        fs.unlinkSync(screenshots + file);
-    }
-}
+    await page.goto("https://popcat.click/");
+    await page.evaluate(() => {
+        for (let i = 0; i < 800; i++) {
+            document.dispatchEvent(new KeyboardEvent('keydown', {
+                key: 'x',
+                ctrlKey: true
+            }));
+        }
+        setInterval(() => {
+            for (let i = 0; i < 800; i++) {
+                document.dispatchEvent(new KeyboardEvent('keydown', {
+                    key: 'x',
+                    ctrlKey: true
+                }));
+            }
+        }, 30 * 1000);
+    });
+    console.log("Started! First pop might be \"Too many request\" don't panic.")
+})();
 
 // Inspired by https://github.com/ckanthony/Popcat-Click-Bot
 // Code written by tawan475
